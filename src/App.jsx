1c04b527d16d91ec9a196a3252c5c6b018caa728
Nikita Harelik <nikitagorelik04@gmail.com>
2025-12-18 02:03:33 +0300

    Update: new component, data; change App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import Section from "./components/HeroesPreview/Section";
 
 const App = () => {
     return (
         <>
             <Header />
+            <Section />
         </>
     );
 };
