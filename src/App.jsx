5a2ff05036d685a2e3088d65f7f42b9f5558d175
Nikita Harelik <nikitagorelik04@gmail.com>
2025-12-20 22:02:36 +0300

    App.jsx: add new section; Header.css: fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Section from "./components/HeroesPreview/Section";
+import MapHeroes from "./components/Map/MapHeroes";
 
 const App = () => {
     return (
         <>
             <Header />
             <Section />
+            <MapHeroes />
         </>
     );
 };
