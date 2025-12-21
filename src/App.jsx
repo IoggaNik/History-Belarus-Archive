93eb17acee1e7f8935b688540203a5b799146795
Nikita Harelik <nikitagorelik04@gmail.com>
2025-12-22 00:43:38 +0300

    Add: function which find coordinates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
+import MainForm from "./components/Forms/MainForm";
 import Header from "./components/Header/Header";
 import Section from "./components/HeroesPreview/Section";
 import MapHeroes from "./components/Map/MapHeroes";
+import FormContext from './components/contexts/formContexts/FormContext'
 
 const App = () => {
+    const [fullName, setFullName] = useState('');
+
     return (
-        <>
+        <FormContext.Provider value={{ fullName, setFullName }}>
             <Header />
             <Section />
             <MapHeroes />
-        </>
+            <MainForm />
+        </FormContext.Provider>
     );
 };
 
---
+++ src/components/Forms/FormAdd/FormAdd.jsx
@@ -0,0 +1,9 @@
+const FormAdd = () => {
+  return (
+    <form>
+      
+    </form>
+  )
+}
+
+export default FormAdd
